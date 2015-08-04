ebda9944040a00f2d6d8c96c0058611ad6a329a1
EC2 Default User <danghoangthien@gmail.com>
2015-08-04 06:42:13 +0000

    move csv upload into project

---
+++ tool/csv_upload.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="./../event_tracking/web/app_dev.php/event/postback/v2" method="post" enctype="multipart/form-data">
+    Select CSV file to upload:
+    <input type="file" name="csv" id="fileToUpload">
+    <input type="submit" value="Upload File" name="submit">
+</form>
+
+</body>
+</html>
