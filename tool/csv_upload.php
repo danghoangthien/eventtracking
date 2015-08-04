7aa088a88f0d2edcce93da81df54958a22a2e349
EC2 Default User <danghoangthien@gmail.com>
2015-08-04 06:48:53 +0000

    use postback version 3

--- tool/csv_upload.php
+++ tool/csv_upload.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<form action="./../event_tracking/web/app_dev.php/event/postback/v2" method="post" enctype="multipart/form-data">
+<form action="web/app_dev.php/event/postback/v3" method="post" enctype="multipart/form-data">
     Select CSV file to upload:
     <input type="file" name="csv" id="fileToUpload">
     <input type="submit" value="Upload File" name="submit">
